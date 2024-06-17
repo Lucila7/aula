fcde206e10e651e33956cf40c30aa1d5541dbc56
Lucila7 <ymluci32@gmail.com>
2024-06-17 04:22:59 -0300

    atividade1

---
+++ atividade1.py
@@ -0,0 +1,5 @@
+nome = input("Digite o nome de usuário: ")
+altura = float(input("Digite a sua altura em metros: "))
+idade = int(input("Digite a sua idade: "))
+
+print(" Seu nome de usuário é ", nome, ", sua altura é ", altura, "metros e a sua idade é ", idade, "anos.")
---
+++ input.py
@@ -0,0 +1,8 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+
+print(type(idade))
+idade_futura = idade + 3
+
+print(f"O nome da pessoa é {nome}, e a idade é {idade}, e a idade formatada é {idade_futura}")
+      
